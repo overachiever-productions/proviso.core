c4807262ddea9e6ef750d860ed5a899708f1c268
Michael K. Campbell <mike@overachiever.net>
2023-02-27 17:52:27 -0800

    Core block definition for Iterator done.

--- public/blocks/Iterator.ps1
+++ public/blocks/Iterator.ps1
@@ -3,20 +3,45 @@
 function Iterator {
 	
 	param (
-		[Parameter(Mandatory)]
-		[PSCustomObject]$Something,
-		[switch]$Strict = $true
+		[Parameter(Mandatory, Position = 0)]
+		[ValidateNotNullOrEmpty()]
+		[string]$Name,
+		[Parameter(Mandatory, Position = 1)]
+		[ScriptBlock]$IteratorBlock,
+		
+		# TODO: this might not even make sense. It's implemented as a STRING for now.
+		[string]$OrderBy = $null
 	);
 	
 	begin {
+		[bool]$xVerbose = ("Continue" -eq $global:VerbosePreference) -or ($PSBoundParameters["Verbose"] -eq $true);
+		[bool]$xDebug = ("Continue" -eq $global:DebugPreference) -or ($PSBoundParameters["Debug"] -eq $true);
 		
+		Enter-Block $MyInvocation.MyCommand -Name $Name -Verbose:$xVerbose -Debug:$xDebug;
 	};
 	
 	process {
+		$definition = New-Object Proviso.Core.Definitions.IteratorDefinition($Name, $true);
 		
+		if (Has-Value $OrderBy) {
+			$definition.OrderBy = $OrderBy;
+		}
+		
+		$definition.Iterate = $IteratorBlock;
+		
+		try {
+			[bool]$replaced = $global:PvCatalog.SetIteratorDefinition($definition, (Allow-DefinitionReplacement));
+			
+			if ($replaced) {
+				Write-Verbose "Iterator block named [$Name] was replaced.";
+			}
+		}
+		catch {
+			throw "$($_.Exception.Message) `r`t$($_.ScriptStackTrace) ";
+		}
 	};
 	
 	end {
-		
+		Exit-Block $MyInvocation.MyCommand -Name $Name -Verbose:$xVerbose -Debug:$xDebug;
 	};
 }
