fe56785a147bb27ef6e3b5b9c08e3333cf9580e0
Michael K. Campbell <mike@overachiever.net>
2023-04-11 14:14:22 -0700

    Bind + Store Logic implemented.
    note: aspects DON'T get stored (ever).

--- public/blocks/Aspect.ps1
+++ public/blocks/Aspect.ps1
@@ -34,7 +34,10 @@ function Aspect {
 						-DisplayFormat $null -Verbose:$xVerbose -Debug:$xDebug;
 		
 		$currentAspect = $definition;
-		Bind-Aspect -Aspect $definition -Verbose:$xVerbose -Debug:$xDebug;
+		
+		# BIND: 
+		Write-Debug "$(Get-DebugIndent)		Binding Aspect: [$($definition.Name)] to Surface: [$($currentSurface.Name)].";
+		$currentSurface.AddAspect($definition);
 		
 		& $ScriptBlock;
 	};
@@ -43,29 +46,4 @@ function Aspect {
 		Exit-Block $MyInvocation.MyCommand -Name $Name -Verbose:$xVerbose -Debug:$xDebug;
 		$global:currentAspect = $null;
 	};
-}
-
-function Bind-Aspect {
-	[CmdletBinding()]
-	param (
-		[Proviso.Core.Definitions.AspectDefinition]$Aspect
-	);
-	
-	process {
-		try {
-			$surfaceName = $global:PvOrthography.GetParentBlockName();
-			$surface = $global:PvOrthography.GetSurfaceDefinition($surfaceName);
-			
-			Write-Debug "$(Get-DebugIndent)		Binding Aspect: [$($Aspect.Name)] to Surface: [$($surfaceName)].";
-			$surface.AddAspect($Aspect);
-			
-			# TODO: do aspects NEED to be stored in the ortho-cache?
-			if ($global:PvOrthography.StoreAspectDefinition($definition, (Allow-DefinitionReplacement))) {
-				Write-Verbose "Aspect: [$Name] was replaced.";
-			}
-		}
-		catch {
-			"Exception in Bind-Aspect: $($_.Exception.Message) `r`t$($_.ScriptStackTrace) ";
-		}
-	}
 }
