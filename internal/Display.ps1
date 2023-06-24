3e4de198f8e67a651f04085fb444881805f73e90
Michael K. Campbell <mike@overachiever.net>
2023-06-24 10:49:33 -0700

    Initial check-in.
    This single file will contain MOST of the 'stuff' needed to handle Display 'stuff'.

---
+++ internal/Display.ps1
@@ -0,0 +1,34 @@
+﻿Set-StrictMode -Version 1.0;
+
+
+# display token 'class/object'
+#  	token/key
+# 	value/object (props)
+# 		.Mapping/Source (i.e., what to run? or what to use to populate the replacement value.  )
+# 		.RequiresCollection 
+# 		.RequiresInstance
+# 		.RequiresSurface 
+# 		.RequiresRunbook 
+
+filter New-PVDisplayToken {
+	param (
+		[string]$Key,
+		[string]$Source,
+		[Switch]$RequiresCollection = $false,
+		[Switch]$RequiresInstance = $false,
+		[Switch]$RequiresSurface = $false,
+		[Switch]$RequiresRunbook = $false # this might not ever even be needed. 
+	);
+	
+	$Token = @{
+		Key = $Key
+		Data = @{
+			RequiresCollection = $RequiresCollection
+			RequiiresInstance  = $RequiresInstance
+			RequiresSurface    = $RequiresSurface
+			RequiresRunbook	= $RequiresRunbook
+		};
+	}
+	
+	return $Token;
+}
